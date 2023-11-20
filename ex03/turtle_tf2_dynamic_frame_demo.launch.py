e5214848404cf84e2ac7768d48c8c9f449da4eba
VChelondaev <104819871+VChelondaev@users.noreply.github.com>
2023-11-20 16:52:14 +0800

    Add files via upload

---
+++ ex03/turtle_tf2_dynamic_frame_demo.launch.py
@@ -0,0 +1,76 @@
+'''import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    demo_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('learning_tf2_py'), 'launch'),
+            '/turtle_tf2_demo.launch.py']),
+            launch_arguments={'target_frame': 'carrot1'}.items(),
+       )
+
+    return LaunchDescription([
+        demo_nodes,
+        Node(
+            package='learning_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+
+        ),
+    ])'''
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename': 'turtle1'}
+            ]
+        ),
+        DeclareLaunchArgument(
+            'target_frame', default_value='turtle1',
+            description='Target frame name.'
+        ),
+        DeclareLaunchArgument(
+            'delay', default_value='5.0',
+            description='Time travel delay'
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame'),
+                 'delay': LaunchConfiguration('delay')}
+            ]
+        )
+    ])
